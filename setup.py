d95e7734b2f79aa30a25f5b3dc0ddd47fdbc4a39
Lars Liedtke <lars@familie-liedtke.net>
2018-09-16 20:07:53 +0200

    TASK: version 0.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='upyls',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/Corvan/upyls',
     license='LGPL v. 2.1',
     author='Lars Liedtke',
